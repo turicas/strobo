7758b9366f6bddae0a0e2252509e24730042a463
Álvaro Justen aka Turicas <alvaro@justen.eng.br>
2013-08-25 08:05:53 -0300

    Version 0.1.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 
 setup(name='strobo',
-      version='0.1.1',
+      version='0.1.2',
       author=u'Álvaro Justen',
       author_email='alvarojusten@gmail.com',
       url='https://github.com/turicas/strobo/',
